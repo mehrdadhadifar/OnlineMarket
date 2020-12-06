5e3a38156e0a4249024ab172d5cf0cc37368c3ce
mehrdadhadifar <mlp8512@gmail.com>
2020-12-06 12:20:22 +0330

    Change orientation of recycler view and fix rtl direction.

--- app/src/main/java/com/hfad/onlinemarket/view/fragment/MainPageFragment.java
+++ app/src/main/java/com/hfad/onlinemarket/view/fragment/MainPageFragment.java
@@ -20,9 +20,12 @@ import com.hfad.onlinemarket.databinding.FragmentMainPageBinding;
 
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Consumer;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 
@@ -31,6 +34,7 @@ public class MainPageFragment extends Fragment {
     FragmentMainPageBinding mBinding;
     WooCommerceAPI mWooCommerceAPI;
     CompositeDisposable mDisposable;
+    ProductAdapter mAdapter;
     private final MutableLiveData<List<Product>> mLatestProductsLiveData = new MutableLiveData<>();
 
 
@@ -63,7 +67,7 @@ public class MainPageFragment extends Fragment {
                 container,
                 false);
 
-        mBinding.newProductsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mBinding.newProductsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, true));
 
 
 /*        mWooCommerceAPI.getAllProducts().enqueue(new Callback<List<Product>>() {
@@ -94,16 +98,37 @@ public class MainPageFragment extends Fragment {
                     }
                 });*/
 
-        mWooCommerceAPI.getProducts(NetworkParams.getProducts(30,2,"date"))
+        Observable<List<Product>> observable = mWooCommerceAPI.getProducts(NetworkParams.getProducts(6, 2, "date"))
                 .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<List<Product>>() {
-                    @Override
-                    public void accept(List<Product> products) {
-                        ProductAdapter adapter = new ProductAdapter(getActivity(), products);
-                        mBinding.newProductsRecyclerView.setAdapter(adapter);
-                    }
-                });
+                .observeOn(AndroidSchedulers.mainThread());
+        Observer<List<Product>> observer = new Observer<List<Product>>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull List<Product> products) {
+                if (mAdapter == null) {
+                    mAdapter = new ProductAdapter(getActivity(), products);
+                    mBinding.newProductsRecyclerView.setAdapter(mAdapter);
+                }else
+                    mBinding.newProductsRecyclerView.notify();
+
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
+        observable.subscribe(observer);
         return mBinding.getRoot();
     }
 }
