c5320082758de3f1d34050971ffad6902f8a9dbc
mehrdadhadifar <mlp8512@gmail.com>
2020-12-10 12:32:43 +0330

    Add navigation.

--- app/src/main/java/com/hfad/onlinemarket/view/activity/MainPageActivity.java
+++ app/src/main/java/com/hfad/onlinemarket/view/activity/MainPageActivity.java
@@ -1,15 +1,22 @@
 package com.hfad.onlinemarket.view.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.DatabaseUtils;
 import android.os.Bundle;
 
+import com.hfad.onlinemarket.R;
+import com.hfad.onlinemarket.databinding.ActivityMainPageBinding;
+import com.hfad.onlinemarket.databinding.FragmentMainPageBinding;
 import com.hfad.onlinemarket.view.fragment.MainPageFragment;
 
-public class MainPageActivity extends SingleFragmentActivity {
+public class MainPageActivity extends AppCompatActivity {
+    private ActivityMainPageBinding mBinding;
 
 
     public static Intent newIntent(Context context) {
@@ -18,9 +25,10 @@ public class MainPageActivity extends SingleFragmentActivity {
     }
 
     @Override
-    public Fragment createFragment() {
-        return MainPageFragment.newInstance();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.Theme_OnlineMarket);
+        super.onCreate(savedInstanceState);
+        mBinding= DataBindingUtil.setContentView(this,R.layout.activity_main_page);
+
     }
-
-
 }
--- app/src/main/java/com/hfad/onlinemarket/view/fragment/ProductDetailsFragment.java
+++ app/src/main/java/com/hfad/onlinemarket/view/fragment/ProductDetailsFragment.java
@@ -2,6 +2,7 @@ package com.hfad.onlinemarket.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,41 +10,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.hfad.onlinemarket.R;
+import com.hfad.onlinemarket.databinding.ListItemProductBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProductDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ProductDetailsFragment extends Fragment {
+    private ListItemProductBinding mBinding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ProductDetailsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProductDetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProductDetailsFragment newInstance(String param1, String param2) {
+
+    public static ProductDetailsFragment newInstance() {
         ProductDetailsFragment fragment = new ProductDetailsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +32,19 @@ public class ProductDetailsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_details, container, false);
+        mBinding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_product_details,
+                container,
+                false
+        );
+
+        return mBinding.getRoot();
     }
 }
